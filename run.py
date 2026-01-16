d7d866ed75e4855b2fd0d6afe2625d8b747a125f
Isaque Weber <isaqueweber@hotmail.com>
2026-01-16 12:01:30 -0300

    Initialize project structure with `.gitignore`, `requirements.txt`, and `run.py` for FastAPI application setup

---
+++ run.py
@@ -0,0 +1,17 @@
+import uvicorn
+import os
+import sys
+
+if __name__ == "__main__":
+    # Garante imports absolutos (PyCharm)
+    sys.path.append(os.getcwd())
+
+    debug = os.getenv("DEBUG", "true").lower() == "true"
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8000)),
+        reload=debug,
+        log_level="debug" if debug else "info"
+    )
